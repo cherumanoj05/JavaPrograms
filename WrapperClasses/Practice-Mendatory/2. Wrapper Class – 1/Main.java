f5a897ec12e048d16fbedb3878a8d316a153c699
cherumanoj05 <59248048+cherumanoj05@users.noreply.github.com>
2022-01-17 20:15:09 +0530

    Create Main.java

---
+++ WrapperClasses/Practice-Mendatory/2. Wrapper Class – 1/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args)  {
+    	System.out.println("Maximum exponent:"+Float.MAX_EXPONENT);
+		System.out.println("Maximum value :"+Float.MAX_VALUE);
+		System.out.println("Number of bits:"+Float.SIZE);
+	}
+}
